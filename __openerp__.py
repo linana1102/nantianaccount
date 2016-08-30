dcb74eec6b6db1cbc043c8f9e2e2237dc38be533
linana <linana@nantian.com.cn>
2016-08-30 15:23:39 +0800

    权限控制

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','mail','hr','project','account'],
+    'depends': ['base','mail','hr','project','account','resource'],
 
     # always loaded
     'data': [
