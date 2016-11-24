af200325f2a7ec2952194c95a94c72e10238ae7b
wanghaipeng28 <wanghaipeng28@163.com>
2016-11-24 14:46:58 +0800

    修改不同状态的背影颜色

--- static/js/nantianErp.js
+++ static/js/nantianErp.js
@@ -1,80 +1,44 @@
 /**
  * Created by Administrator on 2016-09-26.
  */
-console.log("nantianErp模块js文件已经加载。。。。。");
 openerp.nantian_erp=function(instance){
     instance.nantian_erp={};
     var _t=instance.web._t,
         _lt=instance.web._lt,
         QWeb=instance.web.qweb;
-
-    /*instance.web.form.FieldChar = instance.web.form.AbstractField.extend(instance.web.form.ReinitializeFieldMixin, {
-        template: 'test',
-        widget_class: 'oe_form_field_char',
-        events: {
-            'change input': 'store_dom_value',
-        },
-        init: function (field_manager, node) {
-            this._super(field_manager, node);
-            this.password = this.node.attrs.password === 'True' || this.node.attrs.password === '1';
-        },
-        initialize_content: function() {
-            this.setupFocus(this.$('input'));
-        },
-        store_dom_value: function () {
-            if (!this.get('effective_readonly')
-                && this.$('input').length
-                && this.is_syntax_valid()) {
-                this.internal_set_value(
-                    this.parse_value(
-                        this.$('input').val()));
+    instance.nantian_erp.PageLoading=instance.web.Widget.extend({
+        color:null,
+        init:function(){
+            var self=this;
+            this.color={
+                "可调用":"rgba(255,226,50,0.73)",
+                "申请离职":"rgba(255,0,0,0.73)"
             }
         },
-        commit_value: function () {
-            this.store_dom_value();
-            return this._super();
+        start:function(o){
+            this.modifyColor(o.client.action_manager.inner_action.display_name);
         },
-        render_value: function() {
-            var show_value = this.format_value(this.get('value'), '');
-            if (!this.get("effective_readonly")) {
-                this.$el.find('input').val(show_value);
-            } else {
-                if (this.password) {
-                    show_value = new Array(show_value.length + 1).join('*');
-                }
-                this.$(".oe_form_char_content").text(show_value);
+        //调整颜色
+        modifyColor:function(dispalyName){
+            var self=this;
+            if(dispalyName=="待调整的人员"){
+                var timer=setInterval(function(){
+                    if($("div.selfDefColor").length){
+                        clearInterval(timer);
+                        $("div.selfDefColor").each(function(i,v){
+                            var status=$(v).find("span.dis_states").html();
+                            console.log(status);
+                            $(v).css({
+                                "border-color":self.color[status],
+                                "box-shadow":self.color[status]+" 3px 3px 3px 0px"
+                            });
+                        });
+                    }
+                },100);
             }
-        },
-        is_syntax_valid: function() {
-            if (!this.get("effective_readonly") && this.$("input").size() > 0) {
-                try {
-                    this.parse_value(this.$('input').val(), '');
-                    return true;
-                } catch(e) {
-                    return false;
-                }
-            }
-            return true;
-        },
-        parse_value: function(val, def) {
-            return instance.web.parse_value(val, this, def);
-        },
-        format_value: function(val, def) {
-            return instance.web.format_value(val, this, def);
-        },
-        is_false: function() {
-            return this.get('value') === '' || this._super();
-        },
-        focus: function() {
-            var input = this.$('input:first')[0];
-            return input ? input.focus() : false;
-        },
-        set_dimensions: function (height, width) {
-            this._super(height, width);
-            this.$('input').css({
-                height: height,
-                width: width
-            });
         }
-    });*/
+    });
+
+    //当视图加载时调用自己指定代码
+    instance.web.actionList.push(new instance.nantian_erp.PageLoading());
 }
