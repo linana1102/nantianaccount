11362cab9d65c4a599702fc53a1557c2a57f9ac2
linana1102 <971942205@qq.com>
2018-05-17 21:14:47 +0800

    按需求重新设计

--- models/account_analytic.py
+++ models/account_analytic.py
@@ -6,16 +6,20 @@ import datetime
 class employee_sign(models.Model):
     _name = 'nantianaccount.employee_sign'
 
-    date = fields.Date(string='创建日期', default=lambda self: fields.datetime.now(), readonly=True)
+    date = fields.Date(string='创建日期', default=lambda self: fields.datetime.now())
     user_id = fields.Many2one('res.users', default=lambda self: self.env.user, string="创建用户")
     employee_id = fields.Many2one('hr.employee', default=lambda self: self._get_employee_id(), string="员工")
-    from_time = fields.Datetime(string='开始时间')
-    to_time = fields.Datetime(string='结束时间', compute='_count_to_time', store=True)
-    len = fields.Float(string='时长' )
-    working_team_id = fields.Many2one("nantian_erp.working_team", string="项目")
-    exam_user = fields.Many2one('res.users', related="working_team_id.user_id", string="审批人")
+    shangwu_project = fields.Many2one("nantian_erp.working_team", string="上午项目")
+    xiawu_project = fields.Many2one("nantian_erp.working_team", string="下午项目")
+    jiaban = fields.Boolean(string="是否加班")
+    jiaban_end_time = fields.Datetime(string="加班结束时间")
+    jiaban_working_team_id = fields.Many2one("nantian_erp.working_team", string="加班项目")
+    exam_user = fields.Many2one('res.users', string="审批人")
     status = fields.Selection([(u'待审批', u'待审批'), (u'已审批', u'已审批'), (u'已退回' ,u'已退回'), (u'追回', u'追回')], default=u"待审批")
     contract_job_id = fields.Many2one('nantian_erp.jobs', string="合同岗位")
+    shangwu_partner_id =fields.Many2one("res.partner", string="上午客户", domain="[('category','=',u'服务客户')]")
+    xiawu_partner_id = fields.Many2one("res.partner", string="下午客户", domain="[('category','=',u'服务客户')]")
+    jiaban_partner_id = fields.Many2one("res.partner", string="加班客户", domain="[('category','=',u'服务客户')]")
     # contract_id = fields.Many2one("nantian_erp.contract", string="合同")
 
     @api.model
@@ -23,6 +27,16 @@ class employee_sign(models.Model):
         if self.env.user.employee_ids:
             return self.env.user.employee_ids[0]
         return None
+
+    @api.onchange('shangwu_project', 'shangwu_partner_id')
+    def _onchange_price(self):
+        # set auto-changing field
+        self.xiawu_project = self.shangwu_project
+        self.xiawu_partner_id = self.shangwu_partner_id
+        if self.jiaban:
+            self.jiaban_working_team_id = self.shangwu_project
+            self.jiaban_partner_id = self.shangwu_partner_id
+        # Can optionally return a warning and domains
 
     @api.multi
     @api.depends('from_time', 'len')
@@ -33,7 +47,7 @@ class employee_sign(models.Model):
     @api.multi
     def action_to_examine(self):
         self.status = "待审批"
-        self.exam_user = self.working_team_id.user_id
+        # self.exam_user = self.working_team_id.user_id
 
     @api.multi
     def action_to_done(self):
